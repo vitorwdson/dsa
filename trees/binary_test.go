b9c6875f07a962651b500ca2d7446b8fe9e437b0
Vitor Wdson <vitor.wdson2@gmail.com>
2024-04-27 17:29:46 -0400

    feat: implemented binary tree walking

---
+++ trees/binary_test.go
@@ -0,0 +1,31 @@
+package trees
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestBTree(t *testing.T) {
+	h := &BTreeNode[int]{value: 1}
+	h.left = &BTreeNode[int]{value: 2}
+	h.right = &BTreeNode[int]{value: 3}
+	h.left.left = &BTreeNode[int]{value: 4}
+	h.left.right = &BTreeNode[int]{value: 5}
+	h.right.left = &BTreeNode[int]{value: 6}
+	h.right.right = &BTreeNode[int]{value: 7}
+
+	expected := []int{1, 2, 4, 5, 3, 6, 7}
+	if returned := h.WalkPre(); !slices.Equal(returned, expected) {
+		t.Fatalf("WalkPre expected %v, but got %v", expected, returned)
+	}
+
+	expected = []int{4, 5, 2, 6, 7, 3, 1}
+	if returned := h.WalkPost(); !slices.Equal(returned, expected) {
+		t.Fatalf("WalkPost expected %v, but got %v", expected, returned)
+	}
+
+	expected = []int{4, 2, 5, 1, 6, 3, 7}
+	if returned := h.WalkOrdered(); !slices.Equal(returned, expected) {
+		t.Fatalf("WalkOrdered expected %v, but got %v", expected, returned)
+	}
+}
