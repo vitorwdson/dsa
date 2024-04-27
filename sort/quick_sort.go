7f47315b69f15636f4b8d640340cde1bfee81184
Vitor Wdson <vitor.wdson2@gmail.com>
2024-04-27 16:14:16 -0400

    feat: quicksort with pivot on the middle

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -1,38 +1,49 @@
 package sort
 
-func recurseQuickSort(arr []int, lo, hi int) {
+func recurseQuickSort(arr []int, lo, hi, p int) {
 	if (hi - lo) < 1 {
 		return
 	}
 
-	pivot := arr[hi]
+	pValue := arr[p]
 	idx := lo - 1
 
-	for i := lo; i < hi; i++ {
+	for i := lo; i <= hi; i++ {
+		if i == p {
+			continue
+		}
+
 		curr := arr[i]
-		if curr <= pivot {
+		if curr <= pValue {
 			idx++
 			arr[i] = arr[idx]
 			arr[idx] = curr
+
+			if idx == p {
+				p = i
+			}
 		}
 	}
 
 	idx++
-	arr[hi] = arr[idx]
-	arr[idx] = pivot
+	arr[p] = arr[idx]
+	arr[idx] = pValue
 
 	lo1 := lo
 	hi1 := idx - 1
-	recurseQuickSort(arr, lo1, hi1)
+	p1 := lo1 + (hi1-lo1)/2
+	recurseQuickSort(arr, lo1, hi1, p1)
 
 	lo2 := idx + 1
 	hi2 := hi
-	recurseQuickSort(arr, lo2, hi2)
+	p2 := lo2 + (hi2-lo2)/2
+	recurseQuickSort(arr, lo2, hi2, p2)
 }
 
 func QuickSort(arr []int) {
 	hi := len(arr) - 1
 	lo := 0
+	p := hi / 2
 
-	recurseQuickSort(arr, lo, hi)
+	recurseQuickSort(arr, lo, hi, p)
 }
