abeb0be60817dfca772fecda7c4cb47a0998cff3
Vitor Wdson <vitor.wdson2@gmail.com>
2024-04-28 14:12:41 -0400

    feat: bfs without recursion

--- trees/binary_bfs.go
+++ trees/binary_bfs.go
@@ -4,22 +4,20 @@ import (
 	"github.com/vitorwdson/dsa/lists"
 )
 
-func recurseWalkBFS[T any](q lists.Queue[*BTreeNode[T]], seen []T) []T {
-	node, err := q.Pop()
-	if err != nil || node == nil {
-		return seen
-	}
-
-	seen = append(seen, node.value)
-	q.Push(node.left)
-	q.Push(node.right)
-
-	return recurseWalkBFS(q, seen)
-}
-
 func (n *BTreeNode[T]) WalkBFS() []T {
 	q := lists.Queue[*BTreeNode[T]]{}
 	q.Push(n)
 
-	return recurseWalkBFS(q, make([]T, 0))
+	seen := make([]T, 0)
+	for q.Len() > 0 {
+		node, err := q.Pop()
+		if err != nil || node == nil {
+			return seen
+		}
+		seen = append(seen, node.value)
+		q.Push(node.left)
+		q.Push(node.right)
+	}
+
+	return seen
 }
