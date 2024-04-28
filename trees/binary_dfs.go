bc3d02deac782dbf29bcc372b2a04b78962f872d
Vitor Wdson <vitor.wdson2@gmail.com>
2024-04-28 13:50:35 -0400

    feat: separated dfs from base struct file

---
+++ trees/binary_dfs.go
@@ -0,0 +1,49 @@
+package trees
+
+func (n *BTreeNode[T]) recurseWalkPre(visited []T) []T {
+	visited = append(visited, n.value)
+	if n.left != nil {
+		visited = n.left.recurseWalkPre(visited)
+	}
+	if n.right != nil {
+		visited = n.right.recurseWalkPre(visited)
+	}
+
+	return visited
+}
+
+func (n *BTreeNode[T]) WalkPre() []T {
+	return n.recurseWalkPre([]T{})
+}
+
+func (n *BTreeNode[T]) recurseWalkPost(visited []T) []T {
+	if n.left != nil {
+		visited = n.left.recurseWalkPost(visited)
+	}
+	if n.right != nil {
+		visited = n.right.recurseWalkPost(visited)
+	}
+	visited = append(visited, n.value)
+
+	return visited
+}
+
+func (n *BTreeNode[T]) WalkPost() []T {
+	return n.recurseWalkPost([]T{})
+}
+
+func (n *BTreeNode[T]) recurseWalkOrdered(visited []T) []T {
+	if n.left != nil {
+		visited = n.left.recurseWalkOrdered(visited)
+	}
+	visited = append(visited, n.value)
+	if n.right != nil {
+		visited = n.right.recurseWalkOrdered(visited)
+	}
+
+	return visited
+}
+
+func (n *BTreeNode[T]) WalkOrdered() []T {
+	return n.recurseWalkOrdered([]T{})
+}
